74b5b29fdde00f296c9d2940c511b9347067087c
Phong Le <115325732+Phong-H-Le@users.noreply.github.com>
2023-09-29 01:57:36 -0400

    Added Profile / Edit Profile and Random Generation Username with Keyboard Avoiding Layout

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,19 +1,103 @@
-import { StyleSheet , TextInput, Button} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, useColorScheme, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { ScrollView } from 'react-native';
 
-import EditScreenInfo from '../../components/EditScreenInfo';
-import { Text, View } from '../../components/Themed';
 
+export default function TabThreeScreen({}) {
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+  const textColor = isDarkMode ? 'white' : 'black';
+  const [isEditing, setIsEditing] = useState(false);
+  const [username, setUsername] = useState('');
+  const [classYear, setClassYear] = useState('');
+  const [major, setMajor] = useState('');
+  const [hobbies, setHobbies] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState('');
+  const [email, setEmail] = useState('');
 
+  // Function to generate a random username
+  const generateRandomUsername = () => {
+    const adjectives = ['Happy', 'Sad', 'Funny', 'Clever', 'Lucky', 'Graceful'];
+    const nouns = ['Apple', 'Banana', 'Orange', 'Cat', 'Dog', 'Tiger'];
+    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    return `${randomAdjective} ${randomNoun}`;
+  };
 
-export default function TabThreeScreen() {
+  useEffect(() => {
+    setUsername(generateRandomUsername());
+  }, []);
+
+  const handleEditProfile = () => {
+    setIsEditing(true);
+  };
+  // We can add logic here later to send data to Firebase
+  const handleSaveProfile = () => {
+    setIsEditing(false);
+  };
+
+  const handleRegenerateUsername = () => {
+    setUsername(generateRandomUsername());
+  };
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Home</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="app/(tabs)/home.tsx" />
-
-    </View>
-    
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <ScrollView contentContainerStyle={styles.container}>
+      {isEditing ? (
+        <View>
+          <Text style={styles.editProfileTitle}>Edit Profile</Text>
+          <TextInput
+            placeholder="Class Year"
+            value={classYear}
+            onChangeText={(text) => setClassYear(text)}
+            style={[styles.input, { color: textColor }]}
+          />
+          <TextInput
+            placeholder="Major"
+            value={major}
+            onChangeText={(text) => setMajor(text)}
+            style={[styles.input, { color: textColor }]}
+          />
+          <TextInput
+            placeholder="Hobbies"
+            value={hobbies}
+            onChangeText={(text) => setHobbies(text)}
+            style={[styles.input, { color: textColor }]}
+          />
+          <TextInput
+            placeholder="Phone Number"
+            value={phoneNumber}
+            onChangeText={(text) => setPhoneNumber(text)}
+            style={[styles.input, { color: textColor }]}
+          />
+          <TextInput
+            placeholder="Email"
+            value={email}
+            onChangeText={(text) => setEmail(text)}
+            style={[styles.input, { color: textColor }]}
+          />
+          <Button title="Save Profile" onPress={handleSaveProfile} />
+        </View>
+      ) : (
+        <View>
+          <Text style={styles.welcomeTitle}>Welcome, {username}</Text>
+          <FontAwesome.Button
+              name="refresh" // Icon name for the swirly arrow (you can change this)
+              backgroundColor="transparent"
+              onPress={handleRegenerateUsername}
+            />
+          <Text style={[styles.details, { color: textColor }]}>Class Year: {classYear}</Text>
+          <Text style={[styles.details, { color: textColor }]}>Major: {major}</Text>
+          <Text style={[styles.details, { color: textColor }]}>Hobbies: {hobbies}</Text>
+          <Text style={[styles.details, { color: textColor }]}>Phone Number: {phoneNumber}</Text>
+          <Text style={[styles.details, { color: textColor }]}>Email: {email}</Text>
+          <Button title="Edit Profile" onPress={handleEditProfile} />
+        </View>
+      )}
+    </ScrollView>
+    </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -23,13 +107,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  title: {
+  editProfileTitle: {
     fontSize: 20,
     fontWeight: 'bold',
+    color: 'white',
+  },
+  welcomeTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white', 
   },
   separator: {
     marginVertical: 30,
     height: 1,
     width: '80%',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: 250,
+  },
+  details: {
+    fontSize: 16,
+    marginVertical: 5,
   },
 });
