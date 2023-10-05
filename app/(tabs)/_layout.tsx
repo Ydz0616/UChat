4dc89be1c354576127db4e90db77c4cf62131101
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-10-05 10:16:52 -0400

    Removed info icon from header of search page

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -27,20 +27,6 @@ export default function TabLayout() {
         options={{
           title: 'Search',
           tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
-          headerRight: () => (
-            <Link href="/modal" asChild>
-              <Pressable>
-                {({ pressed }) => (
-                  <FontAwesome
-                    name="info-circle"
-                    size={25}
-                    color={Colors[colorScheme ?? 'light'].text}
-                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
-                  />
-                )}
-              </Pressable>
-            </Link>
-          ),
         }}
       />
 
