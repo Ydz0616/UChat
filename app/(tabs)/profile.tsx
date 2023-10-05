da3a292c83dfe2cdfedf644f793d27474ac4b7df
Vedarsh Shah <vedarsh.shah@gmail.com>
2023-10-05 10:12:37 -0400

    Created new EditProfile component

--- app/(tabs)/profile.tsx
+++ app/(tabs)/profile.tsx
@@ -1,15 +1,11 @@
 import React, { useState, useEffect } from 'react';
-import { Alert, StyleSheet, Text, View, Button, TextInput, useColorScheme, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
-import { ScrollView } from 'react-native';
-import {
-  getAuth,
-  signInWithEmailAndPassword ,
-  createUserWithEmailAndPassword,
-  sendEmailVerification
-} from 'firebase/auth';  
+import { Alert, StyleSheet, Button, useColorScheme } from 'react-native';
 import { FIREBASE_APP, FIREBASE_DB, FIREBASE_AUTH } from '../../firebaseConfig';
-import { getFirestore, collection, addDoc, query, where, updateDoc, getDocs } from 'firebase/firestore';
+import { getFirestore, collection, query, where, getDocs } from 'firebase/firestore';
+
+import { Text, View } from '../../components/Themed';
+import EditProfile from '../../components/EditProfile'
+
 export default function TabThreeScreen({}) {
   const firestore = getFirestore(FIREBASE_APP);
   const colorScheme = useColorScheme();
@@ -22,23 +18,12 @@ export default function TabThreeScreen({}) {
   const [hobbies, setHobbies] = useState<string[]>([]);
   const [phoneNumber, setPhoneNumber] = useState('');
   const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [profileSaveCount, setProfileSaveCount] = useState(0);
 
-  
   const auth  = FIREBASE_AUTH;
   const db = FIREBASE_DB;
-  
-  // Function to generate a random username
-  const generateRandomUsername = () => {
-    const adjectives = ['Happy', 'Sad', 'Funny', 'Clever', 'Lucky', 'Graceful'];
-    const nouns = ['Apple', 'Banana', 'Orange', 'Cat', 'Dog', 'Tiger'];
-    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
-    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
-    return `${randomAdjective} ${randomNoun}`;
-  };
 
   useEffect(() => {
-    setUsername(generateRandomUsername());
     const user = auth.currentUser;
     const updateProfileData = async () => {
       try {
@@ -47,11 +32,11 @@ export default function TabThreeScreen({}) {
         );
         if (querySnapshot.docs.length > 0) {
           const userData = querySnapshot.docs[0].data();
+          setUsername(userData.username)
           setClassYear(userData.classYear);
           setMajor(userData.major);
           setHobbies(userData.hobbies); // Convert the array back to a string
           setPhoneNumber(userData.phoneNumber);
-          setPassword(userData.password);
           setEmail(user!.email ?? '');
         }
       } catch (error) {
@@ -60,146 +45,49 @@ export default function TabThreeScreen({}) {
     };
   
     // Call the async function immediately when the component mounts
-    if (user){
+    if (user) {
       updateProfileData();
     }
 
-  }, []);
+  }, [profileSaveCount]);
 
   const handleEditProfile = () => {
     setIsEditing(true);
   };
 
-  const handleSaveProfile = async () => {
+  const handleSaveProfile = () => {
     setIsEditing(false);
-
-
-    try {
-      // Get the current user from Firebase Authentication
-      const user = auth.currentUser;
-
-      // Ensure the user is signed in before proceeding
-      if (user) {
-        setEmail(user.email ?? "");
-        // send email verification if not already verified
-        if (!user.emailVerified) {
-          await sendEmailVerification(user);
-        }
-            // Check if any required field is empty
-        if (!classYear || !major || !hobbies || !phoneNumber) {
-          Alert.alert("Not all of the fields are filled.", "Please fill in the fields.", [
-            {text: "Ok"}
-          ]);
-          return;
-        }
-  
-        // Query Firestore to check if the user's UID already exists
-      const querySnapshot = await getDocs(
-        query(collection(firestore, 'users'), where('uid', '==', user.uid))
-      );
-
-      if (querySnapshot.docs.length > 0) {
-        // User's UID already exists; update the existing document
-        const existingDocRef = querySnapshot.docs[0].ref;
-        await updateDoc(existingDocRef, {
-          username: username.toLowerCase(),
-          classYear: classYear, 
-          major: major.toLowerCase(),
-          hobbies: hobbies.map(hobby => hobby.trim().toLowerCase()), // Assuming hobbies is a comma-separated list
-          phoneNumber: phoneNumber
-        });
-      } 
-      else {
-        // User's UID doesn't exist; create a new document
-        await addDoc(collection(firestore, 'users'), {
-          uid: user.uid,
-          username: username,
-          classYear: classYear,
-          major: major,
-          hobbies: hobbies.map(hobby => hobby.trim()),
-          phoneNumber: phoneNumber,
-          password: password,
-          email: user.email,
-        });
-      }
-        console.log(hobbies.map(hobby => hobby.trim()))
-        console.log(user);
-      } else {
-        console.log("User not signed in.");
-      }
-    } catch (error) {
-      console.log(error);
-    }
+    setProfileSaveCount(profileSaveCount + 1);
   };
-  return (
-    <KeyboardAvoidingView behavior="padding" style={styles.container}>
-    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
-    <ScrollView contentContainerStyle={styles.container}>
-      {isEditing ? (
-        <View>
-          <Text style={styles.editProfileTitle}>Edit Profile</Text>
-          {/* <TextInput
-            placeholder="Email"
-            value={email}
-            onChangeText={(text) => setEmail(text)}
-            style={[styles.input, { color: textColor }]}
-          /> */}
-          {/* <TextInput
-            placeholder="Password"
-            value={password}
-            onChangeText={(text) => setPassword(text)}
-            style={[styles.input, { color: textColor }]}
-          /> */}
-          <TextInput
-            placeholder="Class Year"
-            value={classYear.toString()} // Convert the number to a string for the input value
-            onChangeText={(text) => {
-              const parsedValue = parseInt(text, 10); // Parse the input as an integer
-              if (!isNaN(parsedValue)) {
-                setClassYear(parsedValue);
-              } else {
-                setClassYear(0); // Set a default value if the input is not a valid integer
-              }
-            }}
-            keyboardType="numeric" // Use a numeric keyboard
-            style={[styles.input, { color: textColor }]}
-          />
-          <TextInput
-            placeholder="Major"
-            value={major}
-            onChangeText={(text) => setMajor(text)}
-            style={[styles.input, { color: textColor }]}
-          />
-          <TextInput
-            placeholder="Hobbies"
-            value={hobbies.join(',')}
-            onChangeText={(text) => setHobbies(text.split(','))}
-            style={[styles.input, { color: textColor }]}
-          />
-          <TextInput
-            placeholder="Phone Number"
-            value={phoneNumber}
-            onChangeText={(text) => setPhoneNumber(text)}
-            style={[styles.input, { color: textColor }]}
-          />
-          
-          <Button title="Save Profile" onPress={handleSaveProfile} />
-        </View>
-      ) : (
-        <View>
-          <Text style={styles.welcomeTitle}>Welcome, {username}</Text>
-          <Text style={[styles.details, { color: textColor }]}>Class Year: {classYear}</Text>
-          <Text style={[styles.details, { color: textColor }]}>Major: {major}</Text>
-          <Text style={[styles.details, { color: textColor }]}>Hobbies: {hobbies.join(", ")}</Text>
-          <Text style={[styles.details, { color: textColor }]}>Phone Number: {phoneNumber}</Text>
-          <Text style={[styles.details, { color: textColor }]}>Email: {email}</Text>
-          <Button title="Edit Profile" onPress={handleEditProfile} />
-        </View>
-      )}
-    </ScrollView>
-    </TouchableWithoutFeedback>
-    </KeyboardAvoidingView>
-  );
+
+  const handleCancel = () => {
+    setIsEditing(false)
+  }
+
+  if (isEditing) {
+    return (
+      <EditProfile
+        showCancel={true}
+        handleCancel={handleCancel}
+        saveProfileButtonText="Save Profile"
+        handleSaveProfile={handleSaveProfile}
+      />
+    );
+  }
+  else {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcomeTitle}>Hello, {username}!</Text>
+        <Text style={[styles.details, { color: textColor }]}>Class Year: {classYear}</Text>
+        <Text style={[styles.details, { color: textColor }]}>Major: {major}</Text>
+        <Text style={[styles.details, { color: textColor }]}>Hobbies: {hobbies.join(", ")}</Text>
+        <Text style={[styles.details, { color: textColor }]}>Phone Number: {phoneNumber}</Text>
+        <Text style={[styles.details, { color: textColor }]}>Email: {email}</Text>
+        <Button title="Edit Profile" onPress={handleEditProfile} /> 
+        {/* TODO: Change my email (display alert), change password, log out, delete account */}
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -208,28 +96,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  editProfileTitle: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: 'white',
-  },
   welcomeTitle: {
     fontSize: 20,
     fontWeight: 'bold',
     color: 'white', 
-  },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-  input: {
-    borderWidth: 1,
-    borderColor: 'gray',
-    borderRadius: 5,
-    padding: 10,
-    marginBottom: 10,
-    width: 250,
   },
   details: {
     fontSize: 16,
---
+++ components/EditProfile.tsx
@@ -0,0 +1,222 @@
+import React, { useState, useEffect } from 'react';
+import { Alert, StyleSheet, Button, useColorScheme, KeyboardAvoidingView, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { FontAwesome } from '@expo/vector-icons';
+import { ScrollView } from 'react-native'; 
+import { Text, View, TextInput } from './Themed';
+import { FIREBASE_APP, FIREBASE_DB, FIREBASE_AUTH } from '../firebaseConfig';
+import { getFirestore, collection, addDoc, query, where, updateDoc, getDocs } from 'firebase/firestore';
+
+interface EditProfileProps {
+  showCancel: boolean;
+  handleCancel?: () => void;
+  saveProfileButtonText: string;
+  handleSaveProfile: () => void;
+}
+
+export default function EditProfile(props: EditProfileProps) {
+  const { showCancel, handleCancel, saveProfileButtonText, handleSaveProfile } = props;
+  
+  const firestore = getFirestore(FIREBASE_APP);
+  const colorScheme = useColorScheme();
+  const isDarkMode = colorScheme === 'dark';
+  const textColor = isDarkMode ? 'white' : 'black';
+  const [username, setUsername] = useState('');
+  const [classYear, setClassYear] = useState(0);;
+  const [major, setMajor] = useState('');
+  const [hobbies, setHobbies] = useState<string[]>([]);
+  const [phoneNumber, setPhoneNumber] = useState('');
+  
+  const auth  = FIREBASE_AUTH;
+  const db = FIREBASE_DB;
+  
+  // Function to generate a random username
+  const generateRandomUsername = () => {
+    const adjectives = ['Happy', 'Sad', 'Funny', 'Clever', 'Lucky', 'Graceful'];
+    const nouns = ['Apple', 'Banana', 'Orange', 'Cat', 'Dog', 'Tiger'];
+    const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+    const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+    return `${randomAdjective} ${randomNoun}`;
+  };
+
+  const handleRegenerateUsername = () => {
+    setUsername(generateRandomUsername());
+  };
+
+  useEffect(() => {
+    const user = auth.currentUser;
+    const updateProfileData = async () => {
+      try {
+        const querySnapshot = await getDocs(
+          query(collection(firestore, 'users'), where('uid', '==', user!.uid))
+        );
+        if (querySnapshot.docs.length > 0) {
+          const userData = querySnapshot.docs[0].data();
+          setUsername(userData.username)
+          setClassYear(userData.classYear);
+          setMajor(userData.major);
+          setHobbies(userData.hobbies); // Convert the array back to a string
+          setPhoneNumber(userData.phoneNumber);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  
+    // Call the async function immediately when the component mounts
+    if (user) {
+      updateProfileData();
+    }
+
+  }, []);
+
+  const saveProfile = async () => {
+    try {
+      // Get the current user from Firebase Authentication
+      const user = auth.currentUser;
+
+      // Ensure the user is signed in before proceeding
+      if (user) {        
+        // Check if any required field is empty
+        if (!classYear || !major || !hobbies || !phoneNumber) {
+          Alert.alert("Not all of the fields are filled.", "Please fill in the fields.", [
+            {text: "Ok"}
+          ]);
+          return;
+        }
+  
+        // Query Firestore to check if the user's UID already exists
+      const querySnapshot = await getDocs(
+        query(collection(firestore, 'users'), where('uid', '==', user.uid))
+      );
+
+      if (querySnapshot.docs.length > 0) {
+        // User's UID already exists; update the existing document
+        const existingDocRef = querySnapshot.docs[0].ref;
+        await updateDoc(existingDocRef, {
+          username: username,
+          classYear: classYear,
+          major: major,
+          hobbies: hobbies.map(hobby => hobby.trim()), // Assuming hobbies is a comma-separated list
+          phoneNumber: phoneNumber
+        });
+      } 
+      else {
+        // User's UID doesn't exist; create a new document
+        await addDoc(collection(firestore, 'users'), {
+          uid: user.uid,
+          username: username,
+          classYear: classYear,
+          major: major,
+          hobbies: hobbies.map(hobby => hobby.trim()),
+          phoneNumber: phoneNumber
+        });
+      }
+        console.log(hobbies.map(hobby => hobby.trim()))
+        console.log(user);
+      } else {
+        console.log("User not signed in.");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+
+    handleSaveProfile();
+  };
+  return (
+    <KeyboardAvoidingView behavior="padding" style={styles.container}>
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+    <ScrollView contentContainerStyle={styles.container}>
+    <View>
+        <Text style={styles.editProfileTitle}>Edit Profile</Text>
+        <View style={styles.usernameContainer}>
+          <Text style={styles.username}>{username}</Text>
+          <FontAwesome.Button
+            name="refresh" // Icon name for the swirly arrow (you can change this)
+            backgroundColor="transparent"
+            onPress={handleRegenerateUsername}
+          />
+        </View>
+        
+        <TextInput
+        placeholder="Class Year"
+        value={classYear.toString()} // Convert the number to a string for the input value
+        onChangeText={(text) => {
+            const parsedValue = parseInt(text, 10); // Parse the input as an integer
+            if (!isNaN(parsedValue)) {
+              setClassYear(parsedValue);
+            } else {
+              setClassYear(0); // Set a default value if the input is not a valid integer
+            }
+        }}
+        keyboardType="numeric" // Use a numeric keyboard
+        style={[styles.input, { color: textColor }]}
+        />
+        <TextInput
+        placeholder="Major"
+        value={major}
+        onChangeText={setMajor}
+        style={[styles.input, { color: textColor }]}
+        />
+        <TextInput
+        placeholder="Hobbies"
+        value={hobbies.join(',')}
+        onChangeText={(text) => setHobbies(text.split(','))}
+        style={[styles.input, { color: textColor }]}
+        />
+        <TextInput
+        placeholder="Phone Number"
+        value={phoneNumber}
+        onChangeText={setPhoneNumber}
+        style={[styles.input, { color: textColor }]}
+        />
+        
+        <View style={styles.cancelSaveProfileContainer}>
+          { showCancel ? ( 
+              <Button title="Cancel" color='red' onPress={handleCancel}/>
+          ) : null }
+          { showCancel ? (<View style={{flex: 1}}></View>) : null }
+          <Button title={saveProfileButtonText} onPress={saveProfile}/>
+        </View>
+    </View>
+    </ScrollView>
+    </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  editProfileTitle: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+    width: 250,
+  },
+  usernameContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    marginBottom: 10,
+    width: 250,
+  },
+  username: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    flex: 1
+  },
+  cancelSaveProfileContainer: {
+    flexDirection: "row",
+    justifyContent: "center",
+  }
+});
