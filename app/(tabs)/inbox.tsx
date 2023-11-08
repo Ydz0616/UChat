27c47a14fba44ee64992f440efd18b8d71e66b25
Stephon Ricks <mddev2024@gmail.com>
2023-11-07 21:42:51 -0500

    inbox: listen to changes
    - notifications automatically updated according to live listener
    - no longer necessary to use `refresh` state

--- app/(tabs)/inbox.tsx
+++ app/(tabs)/inbox.tsx
@@ -7,7 +7,6 @@ import { CreateChat } from '../../components/Find';
 
 
 export default function TabOneScreen() {
-  const [refresh, setRefresh] = useState(0)
   const [modalText, setModalText] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
   const [userNotifications, setUserNotifications] = useState<any[]>([])
@@ -16,35 +15,19 @@ export default function TabOneScreen() {
   const fetchData = async () => {
     setUserNotifications([])
     try {
-      
-      // // Fetch their notifications
-      // const requestQuerySnapshot = await getDocs(
-      //   query(collection(FIREBASE_DB, 'notifications'), where('receiver', '==', user!.uid))
-      // );
-
-      // requestQuerySnapshot.forEach((doc) => {
-      //   const request = doc.data();
-      //   if (request?.status === 'pending') {
-      //     setUserNotifications([...userNotifications, request]);
-      //   }
-      // });
-
       const notificationsRef = collection(FIREBASE_DB, 'notifications');
       const queryRef = query(notificationsRef, where('receiver', '==', user!.uid));
-    
-      // Initialize the userNotifications array
-      const initialUserNotifications: any[] = [];
-    
+
       const eventlistener = onSnapshot(queryRef, (querySnapshot) => {
-        const updatedNotifications: any[] = initialUserNotifications;
-    
+        let updatedNotifications: any[] = [];
+
         querySnapshot.forEach((doc) => {
           const request = doc.data() as any; // Type assertion
           if (request?.status === 'pending') {
             updatedNotifications.push(request);
           }
         });
-    
+
         setUserNotifications(updatedNotifications);
       });
       return () => {
@@ -52,13 +35,14 @@ export default function TabOneScreen() {
         eventlistener();
       };
     } catch (error) {
-      console.error(error);
+
     }
   }
 
   useEffect(() => {
     fetchData();
-  }, [refresh]);
+  }, []);
+
 
   const handleUserInfoRequest = async (senderUid: string) => {
     const querySnapshot = await getDocs(
@@ -80,8 +64,8 @@ export default function TabOneScreen() {
       // get the chatting status of both users 
       var chatStatus_sender = (await getDocs(query(collection(FIREBASE_DB, 'users'), where('uid', '==', senderUid)))).docs[0].data().chatting;
       console.log(chatStatus_sender);
-      var chatStatus_receiver = (await getDocs(query(collection(FIREBASE_DB, 'users'), where('uid', '==', receiverUid)))).docs[0].data().chatting;  
-      if (action === 'accepted'&& !chatStatus_receiver && !chatStatus_sender) {
+      var chatStatus_receiver = (await getDocs(query(collection(FIREBASE_DB, 'users'), where('uid', '==', receiverUid)))).docs[0].data().chatting;
+      if (action === 'accepted' && !chatStatus_receiver && !chatStatus_sender) {
         await CreateChat(senderUid)
         await updateDoc(existingDocRef, {
           type: 'friend-request',
@@ -90,18 +74,18 @@ export default function TabOneScreen() {
           status: action,
           timestamp: serverTimestamp()
         });
-        console.log(chatStatus_receiver,chatStatus_sender)
-        Alert.alert('Great job, you just broke the ice!', '', [{text: 'OK', onPress: () => setRefresh(refresh + 1)}])
+        console.log(chatStatus_receiver, chatStatus_sender)
+        Alert.alert('Great job, you just broke the ice!')
       }
-      if (action ==='accepted' && chatStatus_receiver){
-        console.log(chatStatus_receiver,chatStatus_sender)
-        Alert.alert('You are chatting with someone else now', '', [{text: 'OK', onPress: () => setRefresh(refresh + 1)}])
+      if (action === 'accepted' && chatStatus_receiver) {
+        console.log(chatStatus_receiver, chatStatus_sender)
+        Alert.alert('You are chatting with someone else now')
       }
-      if(action ==='accepted' && chatStatus_sender){
-        console.log(chatStatus_receiver,chatStatus_sender)
-        Alert.alert('The sender is chatting with someone else now', '', [{text: 'OK', onPress: () => setRefresh(refresh + 1)}])
+      if (action === 'accepted' && chatStatus_sender) {
+        console.log(chatStatus_receiver, chatStatus_sender)
+        Alert.alert('The sender is chatting with someone else now')
       }
-      if (action === 'rejected'){
+      if (action === 'rejected') {
         await updateDoc(existingDocRef, {
           type: 'friend-request',
           sender: senderUid,
@@ -109,8 +93,8 @@ export default function TabOneScreen() {
           status: action,
           timestamp: serverTimestamp()
         });
-        console.log(chatStatus_receiver,chatStatus_sender)
-        Alert.alert('Rejected friend request', '', [{text: 'OK', onPress: () => setRefresh(refresh + 1)}])
+        console.log(chatStatus_receiver, chatStatus_sender)
+        Alert.alert('Rejected friend request')
       }
     } catch (error) {
       console.error(error)
@@ -137,31 +121,31 @@ export default function TabOneScreen() {
         </View>
       </Modal>
       {userNotifications.length > 0 ?
-      <FlatList
-        data={userNotifications}
-        renderItem={({ item }) => (
-          <View style={styles.resultContainer}>
-            {/* TODO: account for other notification types */}
-            {item.type === 'friend-request' ?
-              <>
-                <Text style={styles.header}>You have a friend request!</Text>
-                <Text style={styles.message}>Ready to break the ice with <Text style={styles.messageHighlight}>{item.sender}</Text>?</Text>
-                <Pressable onPress={() => handleUserInfoRequest(item.sender)}>
-                  <Text style={styles.ref}>Click here to learn more about user</Text>
-                </Pressable>
-                <View style={styles.btngroup}>
-                  <Button title='Accept' onPress={() => handleFriendRequestAction(item.sender, item.receiver, 'accepted')} />
-                  <Button title='Reject' onPress={() => handleFriendRequestAction(item.sender, item.receiver, 'rejected')} />
-                </View>
+        <FlatList
+          data={userNotifications}
+          renderItem={({ item }) => (
+            <View style={styles.resultContainer}>
+              {/* TODO: account for other notification types */}
+              {item.type === 'friend-request' ?
+                <>
+                  <Text style={styles.header}>You have a friend request!</Text>
+                  <Text style={styles.message}>Ready to break the ice with <Text style={styles.messageHighlight}>{item.sender}</Text>?</Text>
+                  <Pressable onPress={() => handleUserInfoRequest(item.sender)}>
+                    <Text style={styles.ref}>Click here to learn more about user</Text>
+                  </Pressable>
+                  <View style={styles.btngroup}>
+                    <Button title='Accept' onPress={() => handleFriendRequestAction(item.sender, item.receiver, 'accepted')} />
+                    <Button title='Reject' onPress={() => handleFriendRequestAction(item.sender, item.receiver, 'rejected')} />
+                  </View>
 
-              </>
+                </>
 
-              : <Text>Not Currently Handled</Text>}
-          </View>
-        )}
-        keyExtractor={item => item.uid}
-      />
-      : <Text style={styles.modalText}>No notifications</Text>}
+                : <Text>Not Currently Handled</Text>}
+            </View>
+          )}
+          keyExtractor={item => item.uid}
+        />
+        : <Text style={styles.modalText}>No notifications</Text>}
     </View>
   );
 }
